865cfb80f80efa013473d423f9070f55b0169cbc
H P O M E N <H P O M E N@LAPTOP-OCOVAM4M>
2024-07-08 23:22:04 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+
+    class program
+    {
+        public enum WeekDays
+        {
+            #region q1
+
+            Saturday,
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday, 
+            #endregion
+
+
+
+
+
+
+
+
+
+
+        }
+        static void Main()
+        {
+
+            #region q1
+          /*  foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+*/
+                #endregion}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+    }
+}     
+ 
+
+
+
+
+
+
+
